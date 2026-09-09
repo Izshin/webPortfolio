72341b231758757bf321829c718098ffbd4f368c
Izshin <114426435+Izshin@users.noreply.github.com>
2026-09-09 16:01:28 +0200

    Update profile.ts

--- portfolioWeb/src/data/profile.ts
+++ portfolioWeb/src/data/profile.ts
@@ -9,7 +9,7 @@ export const profile = {
   linkedin: 'https://www.linkedin.com/in/ivan-fern%C3%A1ndez-lim%C3%A1rquez/',
   linkedinHandle: '@ivaferlim',
   summary:
-    "Hello, I'm Iván! A graduate in Software Engineering passionate about creating clean, user-friendly software. Especially interested in frontend development and UX design, but I have experience in full-stack work. I'm fluent in English and Spanish and always eager to contribute.",
+    "Hello, I'm Iván! A graduate in Software Engineering passionate about creating clean, user-friendly software. Especially interested in frontend development and UX design, but can go full stack as my experience is creating apps end to end. I'm fluent in English and Spanish and always eager to contribute.",
   education: {
     degree: 'B.Sc. in Software Engineering',
     school: 'School of Computer Engineering, University of Seville (Spain)',
